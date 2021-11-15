25ad358a431b648020c9f0bd07eb036b7412b6bb
anusreerajendran31 <60805000+anusreerajendran31@users.noreply.github.com>
2021-11-15 14:21:53 +0530

    new repository

---
+++ css/form.js
@@ -0,0 +1,24 @@
+function validate(){
+    // let username=document.getElementById("user").value;
+    // let password=document.getElementById("pwd").value;
+    // if( username.trim==""|| password.trim==""){
+    //     alert(" Fields cannot be empty.");
+    //     return false;
+    // }
+    // else{
+    //     return true;
+    // }
+}
+
+{/* <script>
+function validatephonenumber(phonenumber){
+    var phoneno = /^\(d{10}$?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if(phonenumber.value.match(phoneno)){
+        return true;
+    }
+    else{
+        alert("Phone number should be in XXX-XXX-XXXX, XXX.XXX.XXXX, XXX XXX XXXX formats only");
+        return false;
+    }
+} 
+</script> */}
